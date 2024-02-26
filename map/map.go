da7f465c293086fb8f47aa577f3b93f8a98aa015
willywin99 <willywinsianturi@gmail.com>
2024-02-26 20:06:51 +0700

    feat: Add Map Exercise

--- map/map.go
+++ map/map.go
@@ -3,13 +3,80 @@ package main
 import "fmt"
 
 func main() {
-	var person map[string]string // Deklarasi
-	person = map[string]string{} // Inisialisasi
-	person["name"] = "Airell"
-	person["age"] = "23"
-	person["address"] = "Jalan Sudirman"
+	// var person map[string]string // Deklarasi
+	// person = map[string]string{} // Inisialisasi
+	// person["name"] = "Airell"
+	// person["age"] = "23"
+	// person["address"] = "Jalan Sudirman"
 
-	fmt.Println("name:", person["name"])
-	fmt.Println("age:", person["age"])
-	fmt.Println("address:", person["address"])
+	// fmt.Println("name:", person["name"])
+	// fmt.Println("age:", person["age"])
+	// fmt.Println("address:", person["address"])
+
+	// var person = map[string]string{
+	// 	"name":    "Airell",
+	// 	"age":     "23",
+	// 	"address": "Jalan Sudirman",
+	// }
+
+	// fmt.Println("name:", person["name"])
+	// fmt.Println("age:", person["age"])
+	// fmt.Println("address:", person["address"])
+
+	// // Map (Looping with Map)
+	// var person = map[string]string{
+	// 	"name":    "Airell",
+	// 	"age":     "23",
+	// 	"address": "Jalan Sudirman",
+	// }
+
+	// for key, value := range person {
+	// 	fmt.Println(key, ":", value)
+	// }
+
+	// // Map (Deleting Value)
+	// var person = map[string]string{
+	// 	"name":    "Airell",
+	// 	"age":     "23",
+	// 	"address": "Jalan Sudirman",
+	// }
+
+	// fmt.Println("Before deleting", person)
+	// delete(person, "age")
+	// fmt.Println("After deleting", person)
+
+	// Map (Detecting a Value)
+	// var person = map[string]string{
+	// 	"name":    "Airell",
+	// 	"age":     "23",
+	// 	"address": "Jalan Sudirman",
+	// }
+
+	// value, exist := person["age"]
+	// if exist {
+	// 	fmt.Println(value)
+	// } else {
+	// 	fmt.Println("Value doesn't exist.")
+	// }
+
+	// delete(person, "age")
+
+	// value, exist = person["age"]
+
+	// if exist {
+	// 	fmt.Println(value)
+	// } else {
+	// 	fmt.Println("Value has been deleted.")
+	// }
+
+	// Combining Slice with Map
+	var people = []map[string]string{
+		{"name": "Airell", "age": "23"},
+		{"name": "Nanda", "age": "23"},
+		{"name": "Mailo", "age": "15"},
+	}
+
+	for i, person := range people {
+		fmt.Printf("Index: %d, name: %s, age: %s\n", i, person["name"], person["age"])
+	}
 }
