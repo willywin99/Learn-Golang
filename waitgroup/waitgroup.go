85f27428da6f414743445511cf9385c35a9040d3
willywin99 <willywinsianturi@gmail.com>
2024-03-08 14:22:00 +0700

    feat: add Waitgroup Exercise

---
+++ waitgroup/waitgroup.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	fruits := []string{"apple", "mango", "durian", "rambutan"}
+	var wg sync.WaitGroup
+	for index, fruit := range fruits {
+		wg.Add(1)
+		go printFruit(index, fruit, &wg)
+	}
+	wg.Wait()
+}
+
+func printFruit(index int, fruit string, wg *sync.WaitGroup) {
+	fmt.Printf("index => %d, fruit => %s\n", index, fruit)
+	wg.Done()
+}
